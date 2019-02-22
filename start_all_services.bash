23cb6ac42f0ff3e2aaca5d9ea9762ec79e0e73e8
Matt Gibbs <mgibbs@slac.stanford.edu>
2019-02-21 17:19:54 -0800

    Last service runs in the foreground so that the container doesn't immediately quit after launching everything.

--- start_all_services.bash
+++ start_all_services.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-(/model_service/model_service.py &) && cd /bpm_service && (python3 bpm_service.py &) && cd /magnet_service && (python3 magnet_service.py &) 
+(/model_service/model_service.py &) && cd /bpm_service && (python3 bpm_service.py &) && cd /magnet_service && python3 magnet_service.py 
