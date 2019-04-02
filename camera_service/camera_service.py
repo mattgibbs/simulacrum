f3383934f79e1eeab09236294d4f3de0c7984428
Ahmed Osman <aosman@rhel6-64b.slac.stanford.edu>
2019-04-02 14:41:34 -0700

    added simple gaussian beam profile image generator

--- camera_service/camera_service.py
+++ camera_service/camera_service.py
@@ -65,13 +65,13 @@ class ProfMonService(simulacrum.Service):
         print("Initialization complete.")
 
     def get_image_size(self, screenProps):
-        screenX = screenProps['values'][0];
-        screenY = screenProps['values'][1];
-        return int(screenX * screenY);
+        screenX = screenProps[0]
+        screenY = screenProps[1]
+        return int(screenX * screenY)
 
     def request_profiles(self):
         self.cmd_socket.send_pyobj({"cmd": "send_profiles_twiss"})
-        return self.cmd_socket.recv_pyobj();
+        return self.cmd_socket.recv_pyobj()
         
     async def recv_profiles(self, flags=0, copy=False, track=False):
         profile_socket = self.ctx.socket(zmq.SUB)
@@ -88,15 +88,16 @@ class ProfMonService(simulacrum.Service):
 
             for row in result:
                 ( _, name, _, _, _, beta_a, beta_b) = row.split();
-                devName = self.ele2dev[name];
+                devName = self.ele2dev[name]
                 if devName not in self.profiles:
                     continue
-                image_size = self.get_image_size(self.profiles[devName]['props']);
 
                 #CGI
-                image = np.ones(image_size)* float(beta_a);
-                self.profiles[devName]['image'] = image.tolist(); 
-
+                print('Calculating image for ' + devName)
+                image = self.gen_beam_image(float(beta_a), float(beta_b), self.profiles[devName]['props']['values'])
+                #image = np.ones(image_size)* float(beta_a);
+                self.profiles[devName]['image'] = image.tolist()
+                print( len(self.profiles[devName]['image']))
             await self.publish_profiles()
 
     async def publish_profiles(self):
@@ -105,10 +106,43 @@ class ProfMonService(simulacrum.Service):
             if pvName in self:
                 try:
                     await self[pvName].write(profile['image'])
-                    print('Publishing profile: ' + key);
+                #    print('ublishing profile: ' + key)
                 except:
                     continue
-    
+
+    # Generate 2D gaussian from orbit & betas.
+    def gen_beam_image(self, beta_a, beta_b, props):
+
+        # image parameters
+        image_size = self.get_image_size(props);
+        cal = (props[3]*1e-6 if props[3] else 1e-10)   # CCD calibration in m/pixel  
+        dimX = props[6]
+        dimY = props[7]
+        centerX = props[10]
+        centerY = props[11]
+        #print("Cal: %f, dimX: %d, dimY: %d, centerX: %d, centerX: %d" % (cal, dimX, dimY, centerX, centerY))
+        # beam parameters
+        emittance = 0.4e-6; 
+        beam_size_x = np.sqrt(beta_a*emittance)
+        beam_size_y = np.sqrt(beta_b*emittance)
+        print(beam_size_x*1e6) 
+        #beam parameters in pixels
+        sig_x = beam_size_x/cal
+        sig_y = beam_size_y/cal
+
+        #normalization of uncorrelated 2D gaussian. TODO: replace random 1000 with a physically sensible number. 
+        A = 1000./np.pi/sig_x/sig_y;
+
+        #generate image. TODO: get particle orbit and offset in x and y
+        x = np.arange(1, dimX+1) - centerX
+        y = np.arange(1, dimY+1) - centerY
+        x2 = -((x/sig_x)**2)/2
+        y2 = -((y/sig_y)**2)/2
+        xx2, yy2 = np.meshgrid(x2, y2)
+        img = A*np.exp(xx2 + yy2)
+        return img.ravel()
+        
+        
 def main():
     service = ProfMonService()
     loop = asyncio.get_event_loop()
