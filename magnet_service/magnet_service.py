9df2d13944d28a58c14b4edbb967e8afdaa31bb7
William Colocho <colocho@slac.stanford.edu>
2020-02-12 09:10:21 -0800

    Add EERR

--- magnet_service/magnet_service.py
+++ magnet_service/magnet_service.py
@@ -24,6 +24,7 @@ class MagnetPV(PVGroup):
     blem = pvproperty(value=0.0, name=':BLEM')
     eact = pvproperty(value=0.0, name=':EACT')
     edes = pvproperty(value=0.0, name=':EDES')
+    eerr = pvproperty(value=0.0, name=':EERR')
     bdes_save = pvproperty(value=0.0, name=':BDESSAVE')
     edes_save = pvproperty(value=0.0, name=':EDESSAVE')
     ctrl_strings = ("Ready", "TRIM", "PERTURB", "BCON_TO_BDES", "SAVE_BDES",
